cc295f99d8dc4fdade90d17409fb594bb94365ca
tmt-thao <vika.tran.minh@gmail.com>
2024-12-21 13:16:50 +0100

    Responsive Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
-      Hello World!
+      {/* NAVBAR */}
+      <Navbar/>
+      
+      {/* BREADCRUMB */}
+      {/* INTRODUCTION */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
     </div>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className='w-full h-16 md:h-20 flex items-center justify-between'>
+            {/* LOGO */}
+            <div className='flex items-center gap-4 text-2xl font-bold'>
+                <img src="/logo.png" className='w-8 h-8' alt="" />
+                <span>BlogAway</span>
+            </div>
+
+            {/* MOBILE MENU */}
+            <div className='md:hidden'>
+                {/* MOBILE BUTTON */}
+                <div 
+                    className='cursor-pointer text-4xl'
+                    onClick={() => setOpen((prev) => !prev)}
+                >
+                    {open ? "X" : "≡"}
+                </div>
+
+                {/* MOBILE LINK LIST */}
+                <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 
+                    gap-8 font-medium text-lg bg-[#e6e6ff] transition-all ease-in-out
+                    ${open ? "-right-0" : "-right-[100%]"}`}
+                >
+                    <a href="/">Home</a>
+                    <a href="/">Trending</a>
+                    <a href="/">Most Popular</a>
+                    <a href="/">About</a>
+                    <a href="/">
+                        <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login👋</button>
+                    </a>
+                </div>
+                
+            </div>
+
+            {/* DESKTOP MENU */}
+            <div className='hidden md:flex items-center gap-8 xl:gap-12 font-medium'>
+                <a href="/">Home</a>
+                <a href="/">Trending</a>
+                <a href="/">Most Popular</a>
+                <a href="/">About</a>
+                <a href="/">
+                    <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login👋</button>
+                </a>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar

f161c60b47806c53627242a4b1091adab21ec96f
tmt-thao <vika.tran.minh@gmail.com>
2025-01-01 16:11:29 +0100

    Flowbite installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,19 @@ import SingIn from './pages/SingIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/sign-in" element={<SingIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   )

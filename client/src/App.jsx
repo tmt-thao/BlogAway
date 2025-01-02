3cd1ee6dbf8ef568e6af31898186d434ed9ac466
tmt-thao <vika.tran.minh@gmail.com>
2025-01-02 19:43:35 +0100

    Created Footer Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,9 @@ import SingIn from './pages/SingIn'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
@@ -20,6 +22,7 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
